d9c16f4eefa66fc4efe69fafd987dda6dd61fbdc
Satvik Pendem <satvik.pendem@gmail.com>
2019-07-12 08:44:35 -0400

    Fix CSS in build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 cp -r ~/Notes/blog/posts/* ./blog
-find ./blog -name "*.md" -execdir pandoc -s -c ~/Projects/satvikpendem.com/style.css index.md -o index.html \;
+find ./blog -name "*.md" -execdir pandoc -s -c ../../../style.css index.md -o index.html \;
